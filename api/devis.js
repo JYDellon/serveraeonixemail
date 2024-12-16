d5c26f2844b8ddb7fbc5dd9d22daf2236ef94bab
Dellon <jy.dellon@gmail.com>
2024-12-16 10:51:12 +0100

    maj

--- api/devis.js
+++ api/devis.js
@@ -80,6 +80,103 @@
 
 
 
+// import nodemailer from 'nodemailer';
+// import dotenv from 'dotenv';
+// import Cors from 'cors';
+
+// dotenv.config();
+
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS,
+//   },
+// });
+
+// const cors = Cors({
+//   origin: '*',
+//   methods: ['GET', 'POST'],
+// });
+
+// function runMiddleware(req, res, fn) {
+//   return new Promise((resolve, reject) => {
+//     fn(req, res, (result) => {
+//       if (result instanceof Error) {
+//         return reject(result);
+//       }
+//       return resolve(result);
+//     });
+//   });
+// }
+
+// export default async function handler(req, res) {
+//   await runMiddleware(req, res, cors);
+
+//   if (req.method === 'POST') {
+//     const { prenom, nom, email, telephone, message } = req.body;
+
+//     const mailOptions = {
+//       from: process.env.EMAIL_USER,
+//       to: process.env.EMAIL_DESTINATAIRE,
+//       subject: `Demande de devis de ${prenom} ${nom}`,
+//       text: `
+//         Vous avez reçu une demande de devis :
+//         - Prénom : ${prenom}
+//         - Nom : ${nom}
+//         - Email : ${email}
+//         - Téléphone : ${telephone}
+//         - Message : ${message}
+//       `,
+//     };
+
+//     const confirmationMailOptions = {
+//       from: process.env.EMAIL_USER,
+//       to: email, // Email de l'expéditeur
+//       subject: 'Confirmation de réception de votre demande de devis',
+//       text: `
+//         Bonjour ${prenom} ${nom},
+
+//         Nous avons bien reçu votre demande de devis :
+
+//         "${message}"
+
+//         Nous vous répondrons dans les plus brefs délais.
+
+//         Cordialement,
+//         L'équipe.
+//       `,
+//     };
+
+//     try {
+//       // Envoyer l'email principal
+//       await transporter.sendMail(mailOptions);
+
+//       // Envoyer l'email de confirmation
+//       await transporter.sendMail(confirmationMailOptions);
+
+//       res.status(200).json({ message: 'Email envoyé avec succès!' });
+//     } catch (error) {
+//       console.error('Erreur lors de l\'envoi de l\'email:', error);
+//       res.status(500).json({ error: 'Erreur lors de l\'envoi de l\'email.' });
+//     }
+//   } else {
+//     res.status(405).json({ message: 'Méthode HTTP non autorisée' });
+//   }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
 import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 import Cors from 'cors';
@@ -114,19 +211,43 @@ export default async function handler(req, res) {
   await runMiddleware(req, res, cors);
 
   if (req.method === 'POST') {
-    const { prenom, nom, email, telephone, message } = req.body;
+    const {
+      nom,
+      email,
+      telephone,
+      nomEntreprise,
+      prestation,
+      descriptionBesoins,
+      objectifs,
+      urlSite,
+      fonctionnalites,
+      graphisme,
+      typeProjet,
+      urlSiteRefonte,
+      besoinsHebergement,
+    } = req.body;
 
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to: process.env.EMAIL_DESTINATAIRE,
-      subject: `Demande de devis de ${prenom} ${nom}`,
+      subject: `Nouvelle demande de devis de ${nom}`,
       text: `
-        Vous avez reçu une demande de devis :
+        Vous avez reçu une nouvelle demande de devis :
+        
         - Prénom : ${prenom}
         - Nom : ${nom}
         - Email : ${email}
         - Téléphone : ${telephone}
-        - Message : ${message}
+        - Nom de l'entreprise : ${nomEntreprise || '-'}
+        - Type de projet : ${typeProjet}
+        - Prestation souhaitée : ${prestation}
+        - URL du site (si applicable) : ${urlSite}
+        - URL du site à refondre (si applicable) : ${urlSiteRefonte}
+        - Fonctionnalités souhaitées : ${fonctionnalites || 'Aucune'}
+        - Graphisme spécifique : ${graphisme || 'Non spécifié'}
+        - Description des besoins : ${descriptionBesoins}
+        - Objectifs : ${objectifs}
+        - Besoins pour l'hébergement : ${besoinsHebergement || 'Aucun besoin spécifique'}
       `,
     };
 
@@ -135,13 +256,20 @@ export default async function handler(req, res) {
       to: email, // Email de l'expéditeur
       subject: 'Confirmation de réception de votre demande de devis',
       text: `
-        Bonjour ${prenom} ${nom},
+        Bonjour ${nom},
 
-        Nous avons bien reçu votre demande de devis :
+        Nous avons bien reçu votre demande de devis. Voici un récapitulatif des informations envoyées :
 
-        "${message}"
-
-        Nous vous répondrons dans les plus brefs délais.
+        - Prénom : ${prenom}        
+        - Nom : ${nom}
+        - Téléphone : ${telephone}
+        - Nom de l'entreprise : ${nomEntreprise || '-'}
+        - Type de projet : ${typeProjet}
+        - Prestation souhaitée : ${prestation}
+        - Description des besoins : ${descriptionBesoins}
+        - Objectifs : ${objectifs}
+        
+        Nous vous répondrons dans les plus brefs délais. Si vous avez des questions supplémentaires, n'hésitez pas à nous contacter.
 
         Cordialement,
         L'équipe.
